c4249c6c08ca0c33b4454bcbbbaef515e87f0427
heyabhinav <abhinav0050.be21@chitkara.edu.in>
2023-03-26 23:44:26 +0530

    Updates

---
+++ precache-manifest.bffb76136ed5f96ac256c77beb1724a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "55f2c70d8ce9ae0fabea",
+    "url": "/yt-clone/static/css/main.5fdfb21b.chunk.css"
+  },
+  {
+    "revision": "55f2c70d8ce9ae0fabea",
+    "url": "/yt-clone/static/js/main.b1005a26.chunk.js"
+  },
+  {
+    "revision": "b90b4c5ac57be04c7a71",
+    "url": "/yt-clone/static/js/runtime~main.9928e7c9.js"
+  },
+  {
+    "revision": "99b1961c55d675e491f5",
+    "url": "/yt-clone/static/js/2.8808d590.chunk.js"
+  },
+  {
+    "revision": "c563299317d52c1696cd",
+    "url": "/yt-clone/static/js/3.937f6cc8.chunk.js"
+  },
+  {
+    "revision": "40a2eec3e7097cb649045bcbbba784a5",
+    "url": "/yt-clone/index.html"
+  }
+];
