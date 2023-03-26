492ca82658bb68c37ed9044fd1ad808f7b71696d
heyabhinav <abhinav0050.be21@chitkara.edu.in>
2023-03-26 16:24:04 +0530

    Modified App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
 import Header from './components/Header/Header';
+import Sidebar from './components/Sidebar/Sidebar';
+import Filter from './components/Filter/Filter';
+import Videos from './components/Videos/Videos';
 
 function App() {
   return (
     <>
       <Header/>
-      <h1>Here</h1>
+      <div style={{display:'grid', gridTemplateColumns:'16% 82%', gap: '10px'}}>
+        <div><Sidebar/></div>
+        <div>
+          <div style={{display:'block',backgroundColor:"transparent", height:'2.2em'}}><Filter/></div>
+          <div style={{display:'block',backgroundColor:"transparent"}}><Videos/></div>
+        </div>
+      </div>
     </>
   );
 }
